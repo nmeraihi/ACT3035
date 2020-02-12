33088ce5811cd38d350488a72ed3e73fc97b252e
nmeraihi <nmeraihi@gmail.com>
2020-02-12 10:17:29 -0500

    ajout de la matière du cours 6

--- HIV_2020/scritp_du_cours_6.R
+++ HIV_2020/scritp_du_cours_6.R
@@ -49,6 +49,47 @@ plot_ly(showscale=F) %>%
   )
 
 
+# tracer le plan: 
+
+model_lm <- lm(Sales ~ Radio + TV, data=adv)
+x_grid <- seq(from=min(adv$Radio), to=max(adv$Radio), length=100)
+y_grid <- seq(from=min(adv$TV), to=max(adv$TV), length=200)
+z_grid <- expand.grid(x_grid, y_grid) %>%
+  tbl_df() %>%
+  rename(
+    x_grid = Var1,
+    y_grid = Var2
+  ) %>%
+  mutate(z = coef(model_lm)[1] + coef(model_lm)[2]*x_grid + coef(model_lm)[3]*y_grid) %>%
+  .[["z"]] %>%
+  matrix(nrow=length(x_grid)) %>%
+  t()
+
+plot_ly(showscale=FALSE) %>%
+  add_markers(
+    x = adv$Radio,
+    y = adv$TV,
+    z = adv$Sales,
+    hoverinfo = 'text',
+    text = ~paste("x1 - Radio: ", adv$Radio, "</br> x2 - TV: ", 
+                  adv$TV, "</br> y - Vente: ", adv$Sales)
+  ) %>% 
+  layout(
+    scene = list(
+      xaxis = list(title = "x1 - Radio"),
+      yaxis = list(title = "x2 - TV ($)"),
+      zaxis = list(title = "y - Vente ($)")
+    )
+  ) %>% 
+  add_surface(
+    x = x_grid,
+    y = y_grid,
+    z = z_grid
+  )
+
+
+
+
 
 
 
