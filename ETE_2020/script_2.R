99452d3327278fb54f31bcbf3746fb4b3e8fe94d
nmeraihi <nmeraihi@gmail.com>
2020-05-11 17:59:47 -0400

    ajout di script cours 2

---
+++ ETE_2020/script_2.R
@@ -0,0 +1,18 @@
+vecteur <- c(1,2,3,4,5)
+vecteur <- 1:12
+vecteur
+print(vecteur)
+vecteur_deci <- 12.5:27.8
+vecteur_deci
+
+vect_seq <- seq(12.5,28.8,5)
+vect_seq
+?seq
+example(seq)
+
+vecteur_maqrue_voit <- c("tesla", "bmw", "honda", "toyota", "Nissan")
+vecteur_maqrue_voit[2]
+vecteur_maqrue_voit[c(2,3)]
+vecteur_maqrue_voit[2:4]
+vecteur_maqrue_voit[-c(2,3)]
+vecteur_maqrue_voit[-(2:4)]
