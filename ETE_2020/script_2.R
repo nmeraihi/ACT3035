6ad55c63a9b7fbb141ef10f0841184ff752eeb28
nmeraihi <nmeraihi@gmail.com>
2020-05-11 18:10:21 -0400

    ajout di script cours 2

--- ETE_2020/script_2.R
+++ ETE_2020/script_2.R
@@ -16,3 +16,29 @@ vecteur_maqrue_voit[c(2,3)]
 vecteur_maqrue_voit[2:4]
 vecteur_maqrue_voit[-c(2,3)]
 vecteur_maqrue_voit[-(2:4)]
+
+
+vecteur_prix_voit <- c(55000, 35000, 18000, 23000, 12000)
+
+
+vecteur_voit_autonom <- c(T, F, F, F, F)
+
+vect_port_assur <- c(vecteur_maqrue_voit, 
+                     vecteur_prix_voit,
+                     vecteur_voit_autonom)
+
+vect_port_assur
+
+
+vect_MSRP <- vecteur_prix_voit
+
+vect_MSRP*1.05*1.095
+
+vect_MSRP_tx <- vect_MSRP*1.05*1.095
+
+vect_MSRP_tx == vect_MSRP
+
+all(vect_MSRP_tx == vect_MSRP)
+
+
+
