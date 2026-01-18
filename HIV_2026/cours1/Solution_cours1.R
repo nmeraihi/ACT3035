9ca93ff79da25d2a3d90d2d9ce84f312ebc75154
Samy Senoune <senoune.samy@uqam.ca>
2026-01-18 09:45:59 -0500

    Solution cours #1

---
+++ HIV_2026/cours1/Solution_cours1.R
@@ -0,0 +1,71 @@
+#Exercice #1
+
+#A)
+(3 + 2) * 4
+10 / 2 + 3
+2^3
+
+#B)
+a <- (3 + 2) * 4
+
+#C)
+b <- 10 / 2 + 3
+
+#D)
+a + b
+
+#E)
+rm(b)
+
+
+#Exercice #2 
+
+#A)
+age <- 25
+
+#B)
+class(age)
+
+#C)
+age_char <- as.character(age)
+
+#D)
+class(age_char)
+
+#E)
+as.numeric(age_char)
+
+#F)
+is_student <- TRUE
+
+#G)
+as.numeric(is_student)
+
+
+#Exercice #3 
+
+#A)
+x <- 10
+y <- 20
+
+#B)
+x < y
+
+#C)
+x == y
+
+#D)
+x != y
+
+#E)
+mot1 <- "bonjour"
+mot2 <- "bonjour"
+
+#F)
+mot1 == mot2
+
+#G)
+mot2 <- "Bonsoir"
+mot1 == mot2
+
+
