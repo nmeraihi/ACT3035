ba11c4c4d5e5154d95893363a3dc2b9733e75cf4
Samy Senoune <senoune.samy@uqam.ca>
2026-01-30 13:29:17 -0500

    Exercice fonction

---
+++ HIV_2026/cours4/Exercice_cours_4.R
@@ -0,0 +1,16 @@
+#Exercice fonction ----
+
+## Exercice #1 ---- 
+
+#Créez une fonction qui extrait le deuxieme plus petit élément d'un vecteur numérique
+
+numeric_vec <- c(3,6,12,0,6,50,2,-2)
+
+
+
+##Exercice 2 ----
+# Créez une fonction ,count_e(vecteur), qui compte le nombre d'élément avec au moins un "e"
+
+exemple_char <- c("elephant", "Ecole", "rene", "R", "ensemble")
+
+
