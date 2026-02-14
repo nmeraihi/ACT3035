73f1df1e3e22497e5fd91a07aabb7923c90d3b1d
Samy Senoune <senoune.samy@uqam.ca>
2026-02-14 14:23:17 -0500

    Solution Exercice 4

---
+++ HIV_2026/cours4/Solution_Exercice_4.R
@@ -0,0 +1,42 @@
+#Exercice fonction ----
+
+## Exercice #1 ---- 
+
+#Créez une fonction, min_2(vecteur), qui extrait le deuxieme plus petit élément d'un vecteur numérique
+
+numeric_vec <- c(3,6,12,0,6,50,2,-2)
+
+
+min_2 <- function(vecteur) {
+  vecteur_trie <- sort(vecteur)
+  return(vecteur_trie[2])
+}
+
+min_2(numeric_vec)
+
+
+#Autre facon , version 2
+
+
+
+min_2 <- function(vecteur) {
+  vecteur_nomin <- vecteur[vecteur != min(vecteur)]
+  return(min(vecteur_nomin))
+}
+
+min_2(numeric_vec)
+
+
+
+
+##Exercice 2 ----
+# Créez une fonction ,count_e(vecteur), qui compte le nombre d'éléments avec au moins un "e"
+
+exemple_char <- c("elephant", "Ecole", "rene", "R", "ensemble")
+
+
+count_e <- function(vecteur) {
+  sum(grepl("e", vecteur))
+}
+
+count_e(exemple_char)
