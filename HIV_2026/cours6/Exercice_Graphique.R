6b33f16017061f091017ebcdb67119e3764f430f
Samy Senoune <senoune.samy@uqam.ca>
2026-03-17 21:34:54 -0400

    Exercice graphique

---
+++ HIV_2026/cours6/Exercice_Graphique.R
@@ -0,0 +1,32 @@
+# Exercice 1 – Dataset mtcars
+# Le dataset mtcars contient des informations sur 32 voitures.
+# 
+# 
+# A) Afficher les 8 dernières lignes du dataset.
+# B) Créer un nuage de points entre wt (poids) et mpg (consommation).
+# C) Ajouter une droite de tendance.
+
+data("mtcars")
+
+
+# Exercice 2
+# 
+# A) Créer un graphique montrant : wt en x, mpg en y et la couleur selon le nombre de cylindres cyl
+# B) Changer la forme des points
+
+
+
+
+
+# Exercice 3 – Histogramme avec iris
+# Créer un histogramme de Sepal.Length.
+# Le titre du graphique doit être :  "Distribution de la longueur des sépales"
+# La couleur doit etre "lightblue"
+data("iris")
+
+
+#Exercice 4 – Boxplot
+#Comparer la distribution de Sepal.Length selon l’espèce.
+#Couleur orange, verte et bleu pour les trois boites
+
+
